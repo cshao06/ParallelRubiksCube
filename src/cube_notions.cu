c5511f0b6f76d3936257358f13f7f2c6f8358889
zhitianx <zhitian.xu96@gmail.com>
2019-12-11 13:36:55 -0500

    update __constant__

--- src/cube_notions.cu
+++ src/cube_notions.cu
@@ -19,6 +19,15 @@ __constant__ const uint8_t positions_on_face[kNumFaces][NUM_POSITIONS_PER_FACE] 
   [kR] = {kURF, kUBR, kDRB, kDFR, kUR, kBR, kDR, kFR},
 };
 
+const uint8_t positions_on_face[kNumFaces][NUM_POSITIONS_PER_FACE] = {
+        [kU] = {kURF, kUFL, kULB, kUBR, kUF, kUL, kUB, kUR},
+        [kD] = {kDLF, kDFR, kDRB, kDBL, kDF, kDR, kDB, kDL},
+        [kF] = {kUFL, kURF, kDFR, kDLF, kUF, kFR, kDF, kFL},
+        [kB] = {kUBR, kULB, kDBL, kDRB, kUB, kBL, kDB, kBR},
+        [kL] = {kULB, kUFL, kDLF, kDBL, kUL, kFL, kDL, kBL},
+        [kR] = {kURF, kUBR, kDRB, kDFR, kUR, kBR, kDR, kFR},
+};
+
 __constant__ const uint8_t turn_position[kNumTurns][NUM_POSITIONS_PER_FACE] = {
   [kU_] = {kUFL, kULB, kUBR, kURF, kUL, kUB, kUR, kUF},
   [kUp] = {kUBR, kURF, kUFL, kULB, kUR, kUF, kUL, kUB},
@@ -38,6 +47,27 @@ __constant__ const uint8_t turn_position[kNumTurns][NUM_POSITIONS_PER_FACE] = {
   [kR_] = {kUBR, kDRB, kDFR, kURF, kBR, kDR, kFR, kUR},
   [kRp] = {kDFR, kURF, kUBR, kDRB, kFR, kUR, kBR, kDR},
   [kR2] = {kDRB, kDFR, kURF, kUBR, kDR, kFR, kUR, kBR},
+};
+
+const uint8_t turn_position[kNumTurns][NUM_POSITIONS_PER_FACE] = {
+[kU_] = {kUFL, kULB, kUBR, kURF, kUL, kUB, kUR, kUF},
+[kUp] = {kUBR, kURF, kUFL, kULB, kUR, kUF, kUL, kUB},
+[kU2] = {kULB, kUBR, kURF, kUFL, kUB, kUR, kUF, kUL},
+[kD_] = {kDFR, kDRB, kDBL, kDLF, kDR, kDB, kDL, kDF},
+[kDp] = {kDBL, kDLF, kDFR, kDRB, kDL, kDF, kDR, kDB},
+[kD2] = {kDRB, kDBL, kDLF, kDFR, kDB, kDL, kDF, kDR},
+[kF_] = {kURF, kDFR, kDLF, kUFL, kFR, kDF, kFL, kUF},
+[kFp] = {kDLF, kUFL, kURF, kDFR, kFL, kUF, kFR, kDF},
+[kF2] = {kDFR, kDLF, kUFL, kURF, kDF, kFL, kUF, kFR},
+[kB_] = {kULB, kDBL, kDRB, kUBR, kBL, kDB, kBR, kUB},
+[kBp] = {kDRB, kUBR, kULB, kDBL, kBR, kUB, kBL, kDB},
+[kB2] = {kDBL, kDRB, kUBR, kULB, kDB, kBR, kUB, kBL},
+[kL_] = {kUFL, kDLF, kDBL, kULB, kFL, kDL, kBL, kUL},
+[kLp] = {kDBL, kULB, kUFL, kDLF, kBL, kUL, kFL, kDL},
+[kL2] = {kDLF, kDBL, kULB, kUFL, kDL, kBL, kUL, kFL},
+[kR_] = {kUBR, kDRB, kDFR, kURF, kBR, kDR, kFR, kUR},
+[kRp] = {kDFR, kURF, kUBR, kDRB, kFR, kUR, kBR, kDR},
+[kR2] = {kDRB, kDFR, kURF, kUBR, kDR, kFR, kUR, kBR},
 };
 
 __constant__ const uint8_t turn_orientation[kNumTurns][NUM_POSITIONS_PER_FACE] = {
@@ -61,6 +91,27 @@ __constant__ const uint8_t turn_orientation[kNumTurns][NUM_POSITIONS_PER_FACE] =
   [kR2] = {0, 0, 0, 0, 0, 0, 0, 0},
 };
 
+const uint8_t turn_orientation[kNumTurns][NUM_POSITIONS_PER_FACE] = {
+[kU_] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kUp] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kU2] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kD_] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kDp] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kD2] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kF_] = {2, 1, 2, 1, 1, 1, 1, 1},
+[kFp] = {2, 1, 2, 1, 1, 1, 1, 1},
+[kF2] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kB_] = {2, 1, 2, 1, 1, 1, 1, 1},
+[kBp] = {2, 1, 2, 1, 1, 1, 1, 1},
+[kB2] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kL_] = {2, 1, 2, 1, 0, 0, 0, 0},
+[kLp] = {2, 1, 2, 1, 0, 0, 0, 0},
+[kL2] = {0, 0, 0, 0, 0, 0, 0, 0},
+[kR_] = {2, 1, 2, 1, 0, 0, 0, 0},
+[kRp] = {2, 1, 2, 1, 0, 0, 0, 0},
+[kR2] = {0, 0, 0, 0, 0, 0, 0, 0},
+};
+
 __constant__ const uint8_t edge_manhattan_dist[12][24] = {
   {0, 3, 1, 2, 1, 3, 1, 2, 1, 3, 2, 2, 2, 3, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2},
   {1, 2, 0, 3, 1, 2, 1, 3, 2, 2, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 2, 2, 2},
@@ -74,6 +125,21 @@ __constant__ const uint8_t edge_manhattan_dist[12][24] = {
   {2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 1, 3, 0, 3, 1, 3, 2, 3},
   {2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 3, 1, 3, 0, 3, 1, 3},
   {2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 1, 3, 2, 3, 1, 3, 0, 3},
+};
+
+const uint8_t edge_manhattan_dist[12][24] = {
+{0, 3, 1, 2, 1, 3, 1, 2, 1, 3, 2, 2, 2, 3, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2},
+{1, 2, 0, 3, 1, 2, 1, 3, 2, 2, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 2, 2, 2},
+{1, 3, 1, 2, 0, 3, 1, 2, 2, 3, 2, 2, 1, 3, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1},
+{1, 2, 1, 3, 1, 2, 0, 3, 2, 2, 1, 3, 2, 2, 2, 3, 1, 2, 2, 2, 2, 2, 1, 2},
+{1, 3, 2, 2, 2, 3, 2, 2, 0, 3, 1, 2, 1, 3, 1, 2, 2, 1, 2, 1, 2, 2, 2, 2},
+{2, 2, 2, 3, 2, 2, 1, 3, 1, 2, 0, 3, 1, 2, 1, 3, 1, 2, 2, 2, 2, 2, 1, 2},
+{2, 3, 2, 2, 1, 3, 2, 2, 1, 3, 1, 2, 0, 3, 1, 2, 2, 2, 2, 2, 2, 1, 2, 1},
+{2, 2, 1, 3, 2, 2, 2, 3, 1, 2, 1, 3, 1, 2, 0, 3, 2, 2, 1, 2, 1, 2, 2, 2},
+{2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 1, 2, 2, 2, 2, 2, 0, 3, 1, 3, 2, 3, 1, 3},
+{2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 1, 3, 0, 3, 1, 3, 2, 3},
+{2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 3, 1, 3, 0, 3, 1, 3},
+{2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 1, 3, 2, 3, 1, 3, 0, 3},
 };
 
 __device__ __host__ void TurnCube(uint8_t *cube, uint8_t turn) {
